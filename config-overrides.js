44efe7119f8c052286e7c1dd9d73d78bef90031a
Old-Rain <qlbj949928210@163.com>
2020-08-07 19:02:23 +0800

    项目搭建:rem适配布局

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,17 @@
-const { override, fixBabelImports, addWebpackExternals, addWebpackAlias, addLessLoader, overrideDevServer, watchAll } = require('customize-cra')
 const path = require('path')
+const fs = require('fs')
+
+const {
+  override,
+  fixBabelImports,
+  addWebpackExternals,
+  addWebpackAlias,
+  addLessLoader,
+  overrideDevServer,
+  watchAll,
+} = require('customize-cra')
+const rewirePostcss = require('react-app-rewire-postcss')
+const postcssNormalize = require('postcss-normalize')
 
 /* const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const myPlugin = [
@@ -17,54 +29,75 @@ const myPlugin = [
 module.exports = {
   webpack: override(
     /* fixBabelImports('import', {
-      //配置按需加载
-      libraryName: 'antd',
-      libraryDirectory: 'es',
-      style: true,
-    }), */
+        //配置按需加载
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+      }), */
     /* addWebpackExternals({
-      //不做打包处理配置，如直接以cdn引入的
-      echarts: 'window.echarts',
-      // highcharts:"window.highcharts"
-    }), */
+        //不做打包处理配置，如直接以cdn引入的
+        echarts: 'window.echarts',
+        // highcharts:"window.highcharts"
+      }), */
     addWebpackAlias({
       //路径别名
       '@': path.resolve(__dirname, 'src'),
     }),
     /* addLessLoader({
-      javascriptEnabled: true,
-      modifyVars: {
-        '@primary-color': '#1DA57A',
-      },
-    }), */
+        javascriptEnabled: true,
+        modifyVars: {
+          '@primary-color': '#1DA57A',
+        },
+      }), */
     (config) => {
       /* //暴露webpack的配置 config ,evn
-      // 去掉打包生产map 文件
-      // config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false;
-      if (process.env.NODE_ENV === 'production') config.devtool = false
-      if (process.env.NODE_ENV !== 'development') config.plugins = [...config.plugins, ...myPlugin]
-      //1.修改、添加loader 配置 :
-      // 所有的loaders规则是在config.module.rules(数组)的第二项
-      // 即：config.module.rules[2].oneof  (如果不是，具体可以打印 一下是第几项目)
-      // 修改 sass 配置 ，规则 loader 在第五项(具体看配置)
-      const loaders = config.module.rules.find((rule) => Array.isArray(rule.oneOf)).oneOf
-      loaders[5].use.push({
-        loader: 'sass-resources-loader',
-        options: {
-          resources: path.resolve(__dirname, 'src/asset/base.scss'), //全局引入公共的scss 文件
-        },
-      }) */
+        // 去掉打包生产map 文件
+        // config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false;
+        if (process.env.NODE_ENV === 'production') config.devtool = false
+        if (process.env.NODE_ENV !== 'development') config.plugins = [...config.plugins, ...myPlugin]
+        //1.修改、添加loader 配置 :
+        // 所有的loaders规则是在config.module.rules(数组)的第二项
+        // 即：config.module.rules[2].oneof  (如果不是，具体可以打印 一下是第几项目)
+        // 修改 sass 配置 ，规则 loader 在第五项(具体看配置)
+        const loaders = config.module.rules.find((rule) => Array.isArray(rule.oneOf)).oneOf
+        loaders[5].use.push({
+          loader: 'sass-resources-loader',
+          options: {
+            resources: path.resolve(__dirname, 'src/asset/base.scss'), //全局引入公共的scss 文件
+          },
+        }) */
+      rewirePostcss(config, {
+        plugins: () => [
+          require('postcss-flexbugs-fixes'),
+          require('postcss-preset-env')({
+            autoprefixer: {
+              flexbox: 'no-2009',
+            },
+            stage: 3,
+          }),
+          postcssNormalize(),
+          require('postcss-pxtorem')({
+            rootValue: 100,
+            propWhiteList: ['*'], // 不知道是啥，但是都写上了
+            selectorBlackList: [/ignore/], // 设置忽略转换的类名，可以传入字符串或正则，字符串最终也会生成正则
+            minPixelValue: 3, // 小于该值的不会被转换（等于的也会被转换）
+            mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
+            replace: true, // 是否转换后直接更换属性值
+            exclude: [/node_modules/], // 设置忽略文件，用正则做目录名匹配
+            landscape: false, // 是否处理横屏情况
+          }),
+        ],
+      })
 
-      console.log('webpack', config)
+      // fs.writeFileSync(path.resolve(__dirname, 'config.json'), JSON.stringify(config.module.rules[2].oneOf[3].use), 'utf-8')
 
       return config
     }
   ),
   devServer: overrideDevServer(
-    // dev server plugin
     // watchAll()
     (config) => {
-      console.log('devServer', config)
+      // console.log('devServer', config)
 
       return config
     }
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from './logo.svg'
-import './App.css'
+import styles from './App.module.scss'
 
 import QQ from '@/components/QQ'
 import faviconPng from '@/assets/favicon.png'
@@ -8,22 +8,27 @@ import { chuizi } from '@/utils/dateFormat'
 // const QQ = require('@/components/QQ.tsx')
 
 // const faviconPng = require('@/assets/favicon.png')
-
+console.log(styles)
 function App() {
   chuizi()
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles['App-header']}>
+        <img src={logo} className={styles['App-logo']} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a className={styles['App-link']} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
         <img src={faviconPng} alt="" />
         <QQ />
+        <div className={styles.big}>
+          <div className={[styles.font].join(' ')}>大</div>
+          <div className={[styles.font, styles.ignoreeee].join(' ')}>ignore</div>
+        </div>
+        <div className={styles.line}></div>
       </header>
     </div>
   )
