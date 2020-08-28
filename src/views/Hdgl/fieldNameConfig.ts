ddfce05ebce04b96da15e70baad73df0d855c187
Old-Rain <qlbj949928210@163.com>
2020-08-28 20:59:53 +0800

    mockData

---
+++ src/views/Hdgl/fieldNameConfig.ts
@@ -0,0 +1,85 @@
+
+interface FieldNameConfig {
+  [propName:string]: {
+    crux: string,
+    semantic: string,
+    dtbs: string
+  }
+}
+const fieldNameConfig : FieldNameConfig = {
+  A001: {
+    crux: '',
+    semantic: '',
+    dtbs: '新增时间'
+  },
+  A002: {
+    crux: '',
+    semantic: '',
+    dtbs: '注册时间'
+  },
+  A003: {
+    crux: '',
+    semantic: '',
+    dtbs: '金管家活跃详情'
+  },
+  A004: {
+    crux: '',
+    semantic: '',
+    dtbs: '绑卡时间'
+  },
+
+  B001: {
+    crux: '',
+    semantic: '',
+    dtbs: '阅读详情'
+  },
+  B002: {
+    crux: '',
+    semantic: '',
+    dtbs: '被查看时间'
+  },
+  B003: {
+    crux: '',
+    semantic: '',
+    dtbs: '购买钩子产品详情'
+  },
+  B004: {
+    crux: '',
+    semantic: '',
+    dtbs: '被邀约时间'
+  },
+
+  C001: {
+    crux: '',
+    semantic: '',
+    dtbs: '扫描业务员口袋E详情'
+  },
+  C002: {
+    crux: '',
+    semantic: '',
+    dtbs: '最近的参与产说会时间'
+  },
+  C003: {
+    crux: '',
+    semantic: '',
+    dtbs: '收到建议书详情'
+  },
+  C004: {
+    crux: '',
+    semantic: '',
+    dtbs: '收到投保书详情'
+  },
+  C005: {
+    crux: '',
+    semantic: '',
+    dtbs: '购买长险产品详情'
+  },
+
+  D001: {
+    crux: '',
+    semantic: '',
+    dtbs: '最近的被服务时间'
+  },
+}
+
+export default fieldNameConfig
